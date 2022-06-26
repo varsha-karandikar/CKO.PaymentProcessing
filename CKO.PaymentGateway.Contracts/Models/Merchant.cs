eed74831763ad2e7fc988a4853b07a52f1c5aec9
varsha183 <vjk183@gmail.com>
2022-06-26 19:35:40 +0530

    Added docker image

---
+++ CKO.PaymentGateway.Contracts/Models/Merchant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKO.PaymentGateway.Contracts.Models
+{
+    public class Merchant
+    {
+        public int Id { get; set; }
+
+        public string StoreName { get; set; }
+
+        public string Industry { get; set; }
+
+
+    }
+}
